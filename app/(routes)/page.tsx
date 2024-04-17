f0a14eea0868eccbd6b905c28d55012e44196456
winterlood <king199777@gmail.com>
2024-04-17 13:51:36 +0900

    INIT

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import Test from "@/components/test";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Test />
+    </main>
+  );
+}
